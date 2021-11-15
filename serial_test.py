c92f4b27b58af70e2205db629038c15ea85616f6
Maks <makaleder@gmail.com>
2021-11-15 23:21:57 +0000

    testy

--- serial_test.py
+++ serial_test.py
@@ -2,24 +2,18 @@
 import serial
 import time
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
     ser.reset_input_buffer()
     c = 1
     print(c)
     print("zaczynamy:")
     while True:
-        #c = 1
         line = str(c)  # + "\n"
         ser.write(line.encode('ascii'))
 
         line = ser.readline().decode('ascii').rstrip()
 
         if (line == "1"):
-            # print(line)
             print("nowa wiadomosc")
-            #c = int(line)
             c = c+1
             print(c)
-            #c = c + 1
-            # print(c)
-            # time.sleep(1)
